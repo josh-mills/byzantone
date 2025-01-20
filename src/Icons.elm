ee833e7d59507a8c21b6009b317cbbac00e53063
Joshua Mills <joshua.mills@avetta.com>
2025-01-19 20:55:18 -0500

    hamburger menu icon

--- src/Icons.elm
+++ src/Icons.elm
@@ -1,4 +1,4 @@
-module Icons exposing (xmark)
+module Icons exposing (bars, xmark)
 
 {-| TODO: this should be generated code. We should probably also consider the
 API a bit; might want to add in additional attributes.
@@ -7,6 +7,15 @@ API a bit; might want to add in additional attributes.
 import Html exposing (Html)
 import Svg exposing (path, svg)
 import Svg.Attributes exposing (d, viewBox)
+
+
+bars : Html msg
+bars =
+    svg [ viewBox "0 0 448 512" ]
+        [ path
+            [ d "M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" ]
+            []
+        ]
 
 
 xmark : Html msg
