a2acca3e352dbdb6c671df8a43e0cda7eeea8c87
Joshua Mills <joshua.mills928@gmail.com>
2025-09-16 17:01:28 -0400

    add chevron icons

--- src/Icons.elm
+++ src/Icons.elm
@@ -1,4 +1,4 @@
-module Icons exposing (bars, gear, microphone, microphoneSlash, music, sliders, triangleExclamation, volumeHigh, volumeLow, volumeOff, xmark)
+module Icons exposing (bars, chevronDown, chevronLeft, chevronRight, chevronUp, gear, microphone, microphoneSlash, music, sliders, triangleExclamation, volumeHigh, volumeLow, volumeOff, xmark)
 
 {-| Font Awesome SVG icons with customizable attributes.
 
@@ -28,6 +28,42 @@ bars attributes =
     svg (viewBox "0 0 448 512" :: attributes)
         [ path
             [ d "M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" ]
+            []
+        ]
+
+
+chevronDown : List (Svg.Attribute msg) -> Html msg
+chevronDown attributes =
+    svg (viewBox "0 0 640 640" :: attributes)
+        [ path
+            [ d "M297.4 470.6C309.9 483.1 330.2 483.1 342.7 470.6L534.7 278.6C547.2 266.1 547.2 245.8 534.7 233.3C522.2 220.8 501.9 220.8 489.4 233.3L320 402.7L150.6 233.4C138.1 220.9 117.8 220.9 105.3 233.4C92.8 245.9 92.8 266.2 105.3 278.7L297.3 470.7z" ]
+            []
+        ]
+
+
+chevronLeft : List (Svg.Attribute msg) -> Html msg
+chevronLeft attributes =
+    svg (viewBox "0 0 640 640" :: attributes)
+        [ path
+            [ d "M169.4 297.4C156.9 309.9 156.9 330.2 169.4 342.7L361.4 534.7C373.9 547.2 394.2 547.2 406.7 534.7C419.2 522.2 419.2 501.9 406.7 489.4L237.3 320L406.6 150.6C419.1 138.1 419.1 117.8 406.6 105.3C394.1 92.8 373.8 92.8 361.3 105.3L169.3 297.3z" ]
+            []
+        ]
+
+
+chevronRight : List (Svg.Attribute msg) -> Html msg
+chevronRight attributes =
+    svg (viewBox "0 0 640 640" :: attributes)
+        [ path
+            [ d "M471.1 297.4C483.6 309.9 483.6 330.2 471.1 342.7L279.1 534.7C266.6 547.2 246.3 547.2 233.8 534.7C221.3 522.2 221.3 501.9 233.8 489.4L403.2 320L233.9 150.6C221.4 138.1 221.4 117.8 233.9 105.3C246.4 92.8 266.7 92.8 279.2 105.3L471.2 297.3z" ]
+            []
+        ]
+
+
+chevronUp : List (Svg.Attribute msg) -> Html msg
+chevronUp attributes =
+    svg (viewBox "0 0 640 640" :: attributes)
+        [ path
+            [ d "M297.4 169.4C309.9 156.9 330.2 156.9 342.7 169.4L534.7 361.4C547.2 373.9 547.2 394.2 534.7 406.7C522.2 419.2 501.9 419.2 489.4 406.7L320 237.3L150.6 406.6C138.1 419.1 117.8 419.1 105.3 406.6C92.8 394.1 92.8 373.8 105.3 361.3L297.3 169.3z" ]
             []
         ]
 
